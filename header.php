cb23a5bc2caaa8383ac7222556e93430829640f3
Natsuko <34265344+Namiro74@users.noreply.github.com>
2019-12-06 02:23:31 +0100

    deleted old file

--- header.php
+++
@@ -1,7 +0,0 @@
-<header>
-	<div class="titre"><h1><a href="/ndi/index.php">Plouf, à l'aide !</a></h1></div>
-	<div class="conteneur">
-		<div class="element"><a class="IC" href="connexion.php"><h1>Connexion</h1></a></div>
-		<div class="element"><a class="IC" href="inscription.php"><h1>Inscription</h1></a></div>
-	</div>
-</header>
